431ce47bdae90f0578808f5914807e19cbb975ae
Anda Ben <anben@student.wethinkcode.co.za>
2026-01-01 02:08:32 +0200

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ import { restartEnvFileChange } from './plugins/restartEnvFileChange';
 export default defineConfig({
   // Keep them available via import.meta.env.NEXT_PUBLIC_*
   envPrefix: 'NEXT_PUBLIC_',
-  base: "/portfolio-website",
+  base: "/portfolio-website/",
   
   optimizeDeps: {
     // Explicitly include fast-glob, since it gets dynamically imported and we
